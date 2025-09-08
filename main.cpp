635ff647c7f9644bf881f33362e8c39711273ef0
Alex-115 <alexxjr.2828@gmail.com>
2025-09-08 11:27:00 -0700

    Closes #166

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-using std::cin;
-using std::cout;
-using std::endl;
-
+using namespace std;
 int main()
 {
     cout << "Hi, please enter two whole numbers: ";
